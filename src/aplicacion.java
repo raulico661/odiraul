4d42d8042e48d252525620f24454b20f6484f72c
Raúl <raulelche2001@gmail.com>
2022-11-11 20:07:47 +0100

    Primera versión

---
+++ src/aplicacion.java
@@ -0,0 +1,16 @@
+public class aplicacion {
+    public static void main(String[] args) {
+
+        System.out.println("Estudiante " + args[0]);
+    }
+
+    public static String veredicto(String valor1, String valor2){
+        if (valor1.equals(valor2)) {
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+            return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+    }
+}
